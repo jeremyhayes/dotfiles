76429d54b7b589199bf35f9b99b556029cde0b48
Jeremy Hayes <jeremyhayes@github.com>
2021-10-27 11:53:00 -0400

    add zsh and powerlevel10k theme

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,3 +14,7 @@ link_file() {
 
 link_file ".gitconfig"
 link_file ".vimrc"
+link_file ".zshrc"
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+link_file ".p10k.zsh"
